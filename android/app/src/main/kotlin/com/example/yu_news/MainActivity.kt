29f3da8c2e5c7d76f05443f157719e03787b8a4d
yuvraj uppal <yuvrajuppal211@gmail.com>
2025-03-23 06:09:46 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/yu_news/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yu_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.red
+        ),
+      )
+    );
+  }
+}
