daeaffc2630e5ea55df8bb3cd2913327bbcb5548
yuvraj uppal <yuvrajuppal211@gmail.com>
2025-03-25 14:18:39 +0530

    home page ui updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,247 @@ class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('images/onbording.jpg'),
+      backgroundColor: const Color.fromARGB(221, 255, 255, 255),
+      body: Container(
+        margin: EdgeInsets.only(top: 55, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Yu",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "News",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                
+              ],
+              
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Hottest News",
+              style: TextStyle(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Container(
+              height: MediaQuery.of(context).size.height/2.6,
+              child: ListView(
+                scrollDirection:  Axis.horizontal,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Material(
+                      elevation: 4.0,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                      ),
+                      child: Container(
+                        
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                            bottomLeft: Radius.circular(15),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              padding: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  'images/demonews.jpg',
+                                  
+                                  width: MediaQuery.of(context).size.width / 1.8,
+                                  
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              padding: EdgeInsets.only(left: 5, right: 5),
+                              child: Text(
+                                "Tech Giant Unveils AI-Powered Smartphone..",
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 19.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              padding: EdgeInsets.only(left: 5, right: 5),
+                                  
+                              child: Text(
+                                "In a groundbreaking move, a leading tech company has ",
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 95, 95, 95),
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Spacer(),
+                            Container(
+                              padding: EdgeInsets.all(0),
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    width: 80,
+                                    height: 40,
+                                    margin: EdgeInsets.only(right: 0),
+                                  
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                      ),
+                                    ),
+                                    child: Icon(Icons.arrow_forward, color: Colors.white, size: 30,),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+                  SizedBox(width: 15,),
+                    
+
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Material(
+                      elevation: 4.0,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                      ),
+                      child: Container(
+                        
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                            bottomLeft: Radius.circular(15),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              padding: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  'images/demonews.jpg',
+                                  
+                                  width: MediaQuery.of(context).size.width / 1.8,
+                                  
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              padding: EdgeInsets.only(left: 5, right: 5),
+                              child: Text(
+                                "Tech Giant Unveils AI-Powered Smartphone..",
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 19.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              padding: EdgeInsets.only(left: 5, right: 5),
+                                  
+                              child: Text(
+                                "In a groundbreaking move, a leading tech company has ",
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 95, 95, 95),
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Spacer(),
+                            Container(
+                              padding: EdgeInsets.all(0),
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    width: 80,
+                                    height: 40,
+                                    margin: EdgeInsets.only(right: 0),
+                                  
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                      ),
+                                    ),
+                                    child: Icon(Icons.arrow_forward, color: Colors.white, size: 30,),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -12,12 +12,86 @@ class _OnboardingState extends State<Onboarding> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          color: Colors.red
+        decoration: BoxDecoration(color: const Color.fromARGB(255, 1, 1, 1)),
+        child: Stack(
+          children: [
+            Image.asset(
+              'images/newspapercoffeeimage.jpg',
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.7,
+              fit: BoxFit.cover,
+            ),
+
+            Container(
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 1.9,
+              ),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text(
+                      'Get The Latest And Updated\nNews Easily With Us',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+
+                  Image.asset(
+                    "images/newpaperEmoji.png",
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+
+                  SizedBox(height: 20),
+                  Container(
+                    margin: EdgeInsets.only(left: 40, right: 40),
+
+                    child: Material(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      elevation: 6.0,
+                      child: Container(
+                        height: 50,
+               
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(
+                            'Get Started',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
